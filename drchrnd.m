ee444302c6f7deeb3d7b10e3200d23a77665bc8d
Runze Tang <tangrunze@gmail.com>
2014-11-04 22:47:31 -0500

    Dirichlet sampling code added.

---
+++ drchrnd.m
@@ -0,0 +1,5 @@
+function r = drchrnd(a,n)
+% Using Gamma distribution to generate samples from Dirichlet distribution.
+p = length(a);
+r = gamrnd(repmat(a,n,1),1,n,p);
+r = r ./ repmat(sum(r,2),1,p);
