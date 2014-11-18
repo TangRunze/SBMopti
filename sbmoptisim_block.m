a54f854d1545e145e2ff0924b169b8511c903695
Runze Tang <tangrunze@gmail.com>
2014-11-17 23:16:02 -0500

    Revise sbmoptisim_block to handle log-likelihood.

--- sbmoptisim_block.m
+++ sbmoptisim_block.m
@@ -91,7 +91,7 @@ end
 
 %% --- Optimization Setting ---
 options = optimoptions('fmincon', 'TolX', 1e-6, ...
-    'MaxIter', 10000, 'MaxFunEvals', 10000, ...
+    'MaxIter', 1000, 'MaxFunEvals', 1000, ...
     'Algorithm', 'interior-point'); %, 'GradObj', 'on');
 % Algorithm: 'interior-point', 'trust-region-reflective', 'sqp', 'active-set'
 projectoptions = optimoptions('fmincon', 'TolX', 1e-6, 'MaxIter', ...
@@ -101,17 +101,18 @@ projectoptions = optimoptions('fmincon', 'TolX', 1e-6, 'MaxIter', ...
 delete(gcp('nocreate'))
 parpool(nCore);
 
-lambdaVec = [0.001 0.01 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0.99 0.999];
+lambdaVec = [0.001 0.01 0.5 0.8 0.9 0.95 0.99 0.995 0.999];
+% lambdaVec = [0.5];
 
-parfor iProbMatrix = gStart:gEnd
+parfor iGraph = gStart:gEnd
     
     %% --- Generate/Read Data ---
     % Generate data if there does not exist one, otherwise read the
     % existing data.
-    [pMatrix, nuHat0, ~, tauHat0, ~] = ...
-        datagenerator_block(nVertex, nBlock, dimLatentPosition, B, rho, ...
-        tauStar, epsilonInB, r, iProbMatrix, projectoptions);
-    xHat = asge(pMatrix, dimLatentPosition);
+    [adjMatrix, nuHat0, ~, tauHat0, ~] = ...
+        datagenerator_block_A(nVertex, nBlock, dimLatentPosition, B, rho, ...
+        tauStar, epsilonInB, r, iGraph, projectoptions);
+    xHat = asge(adjMatrix, dimLatentPosition);
     
     % Pre-projection
     rotationMatrix = fmincon(@(x) projectobjectivefun(x, ...
@@ -136,7 +137,7 @@ parfor iProbMatrix = gStart:gEnd
         saveFile = ['./results/results-SBMopti-Block-sim-dir-n' ...
             num2str(nVertex) '-diag' num2str(B(1, 1)) '-offdiag' ...
             num2str(B(1, 2)) '-r' num2str(r) '-lambda' num2str(lambda) ...
-            '-pmatrix' num2str(iProbMatrix) '.mat'];
+            '-adjMatrix' num2str(iGraph) '.mat'];
         
         if exist(saveFile, 'file') == 0
             %% --- Solve Optimization Problem ---
@@ -148,14 +149,14 @@ parfor iProbMatrix = gStart:gEnd
             tauHat = tauHat0;
             
             fValueOld = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-                pMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
+                adjMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
             
             while (~hasConverge) && (iter < maxIter)
                 iter = iter + 1
                 
                 % Find the best xHatTmp based on current nuHat, tauHat.
                 xHatTmp = fmincon(@(x) objectivefun_std(x, ...
-                    pMatrix, nuHat, lambda, nVertex, dimLatentPosition, ...
+                    adjMatrix, nuHat, lambda, nVertex, dimLatentPosition, ...
                     tauHat), reshape(xHatTmp, 1, nVertex*dimLatentPosition),...
                     [], [], [], [], zeros(dimLatentPosition*nVertex, 1), ...
                     ones(dimLatentPosition*nVertex, 1), @(x) ...
@@ -167,18 +168,12 @@ parfor iProbMatrix = gStart:gEnd
                 tauHat = tauHat';
                 
                 fValueNew = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-                    pMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
+                    adjMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
                 if (fValueNew < fValueBest) || (iter == 1)
                     fValueBest = fValueNew;
                     tauBest = tauHat;
                     xBest = xHatTmp;
                     nuBest = nuHat;
-                    
-                    fValue0 = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-                        pMatrix, nuHat, 0, nVertex, dimLatentPosition, tauHat);
-                    fValue1 = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-                        pMatrix, nuHat, 1, nVertex, dimLatentPosition, tauHat);
-                    
                 end
                 
                 % Convergence Checking
@@ -195,87 +190,23 @@ parfor iProbMatrix = gStart:gEnd
                     nVertex, nBlock);
                 errorRateOptiBest = errorratecalculator(tauStar, tauBest, ...
                     nVertex, nBlock);
+                % Calculate the log-likelihood of the solution
+                loglik = 0;
+                tmpB = nuBest*nuBest';
+                for i = 1:(nVertex - 1)
+                    for j = (i+1):nVertex
+                        loglik = loglik + adjMatrix(i, j)*...
+                            log(tmpB(tauBest(i), tauBest(j))) + ...
+                            (1 - adjMatrix(i, j))*...
+                            log(1 - tmpB(tauBest(i), tauBest(j)));
+                    end
+                end
                 % parsave(saveFile, errorRateASGE, errorRateOpti, errorRateOptiBest, scatterdata);
                 parsave(saveFile, errorRateASGE, errorRateOpti, ...
                     errorRateOptiBest, fValueNew, fValueBest, ...
-                    xBest, nuBest, tauBest, fValue0, fValue1);
+                    xBest, nuBest, tauBest, loglik);
             end
         end
-    end
-    
-    %% Consider lambda = 0
-    lambda = 0;
-    
-    saveFile = ['./results/results-SBMopti-Block-sim-dir-n' ...
-            num2str(nVertex) '-diag' num2str(B(1, 1)) '-offdiag' ...
-            num2str(B(1, 2)) '-r' num2str(r) '-lambda' num2str(lambda) ...
-            '-pmatrix' num2str(iProbMatrix) '.mat']
-    
-    if exist(saveFile, 'file') == 0
-        
-        %% --- Solve Optimization Problem ---
-        xHatTmp = xHat;
-        
-        [tauHat, nuHat] = kmeans(xHatTmp, nBlock);
-        tauHat = tauHat';
-        
-        fValueNew = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-            pMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
-        fValueBest = fValueNew;
-        
-        fValue0 = fValueBest;
-        fValue1 = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-            pMatrix, nuHat, 1, nVertex, dimLatentPosition, tauHat);
-        
-        %% --- Save Results ---
-        errorRateOpti = errorratecalculator(tauStar, tauHat, ...
-            nVertex, nBlock);
-        errorRateOptiBest = errorRateOpti;
-        parsave(saveFile, errorRateASGE, errorRateOpti, ...
-            errorRateOptiBest, fValueNew, fValueBest, ...
-            xHatTmp, nuHat, tauHat, fValue0, fValue1);
-    end
-    
-    %% Consider lambda = 1
-    lambda = 1;
-    
-    saveFile = ['./results/results-SBMopti-Block-sim-dir-n' ...
-        num2str(nVertex) '-diag' num2str(B(1, 1)) '-offdiag' ...
-        num2str(B(1, 2)) '-r' num2str(r) '-lambda' num2str(lambda) ...
-        '-pmatrix' num2str(iProbMatrix) '.mat'];
-    
-    if exist(saveFile, 'file') == 0
-        
-        %% --- Solve Optimization Problem ---
-        nuHat = nuHat0;
-        tauHat = tauHat0;
-        
-        % Find the best xHatTmp based on current nuHat, tauHat.
-        nuHat = fmincon(@(x) objectivefun_std_lambda1(...
-            pMatrix, x, dimLatentPosition, nBlock, tauHat), ...
-            reshape(nuHat, 1, nBlock*dimLatentPosition),...
-            [], [], [], [], zeros(dimLatentPosition*nBlock, 1), ...
-            ones(dimLatentPosition*nBlock, 1), @(x) ...
-            conditionfun(x, nBlock, dimLatentPosition), options);
-        nuHat = reshape(nuHat, nBlock, dimLatentPosition);
-
-        xHatTmp = nuHat(tauHat, :);
-        
-        fValueNew = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-            pMatrix, nuHat, lambda, nVertex, dimLatentPosition, tauHat);
-        fValueBest = fValueNew;
-        
-        fValue1 = fValueBest;
-        fValue0 = objectivefun_std(reshape(xHatTmp, 1, nVertex*dimLatentPosition), ...
-            pMatrix, nuHat, 0, nVertex, dimLatentPosition, tauHat);
-        
-        %% --- Save Results ---
-        errorRateOpti = errorratecalculator(tauStar, tauHat, ...
-            nVertex, nBlock);
-        errorRateOptiBest = errorRateOpti;
-        parsave(saveFile, errorRateASGE, errorRateOpti, ...
-            errorRateOptiBest, fValueNew, fValueBest, ...
-            xHatTmp, nuHat, tauHat, fValue0, fValue1);
     end
 end
 
